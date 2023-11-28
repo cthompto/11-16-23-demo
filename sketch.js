f05befcc11c437bdebce5dd8a2e758e440b9710d
Chelsea Thompto <chelsea.l.thompto@gmail.com>
2023-11-28 17:04:08 -0500

    added cnosole.log

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
 function setup() {
   createCanvas(400, 400);
+  console.log("Hello Hello!");
 }
 
 function draw() {
   background(220);
+  rect(50,50,10);
+  rect(200,200,20);
+  rect(20,300,10);
 }
